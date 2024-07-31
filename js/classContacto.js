e743e57680abf487c441e02eb15d39d84178abab
Emilse Arias <emilsearias@gmail.com>
2024-07-31 10:28:56 -0300

    creamos la clase Contacto

---
+++ js/classContacto.js
@@ -0,0 +1,24 @@
+class Contacto{
+    #id;
+    #nombre;
+    #apellido;
+    #apodo;
+    #direccion;
+    #celular;
+    #email;
+    #foto
+    constructor(id ,nombre, apellido, apodo, direccion, celular, email, foto){
+        this.#id= id;
+        this.#nombre= nombre;
+        this.#apellido = apellido;
+        this.#apodo = apodo;
+        this.#direccion = direccion;
+        this.#celular = celular
+        this.#email = email;
+        this.#foto = foto;
+    }
+
+    // agregar los get y set de cada propiedad
+
+    // agregar los metodos necesarios
+}
