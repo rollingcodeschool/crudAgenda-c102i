9d2635ded2d42fef456084c431c3fc6f91b2f6f3
Emilse Arias <emilsearias@gmail.com>
2024-08-05 09:58:06 -0300

    abrimos la ventana modal de registro de contacto desde el admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,10 +1,27 @@
 import Contacto from "./classContacto.js";
 
-console.log("prueba desde admin");
+// crear variables
 const contactoNuevo = new Contacto(undefined,'Rosario', 'Pittaro', 'Ro','algun lugar','381566777', 'rosario@mail.com','link')
-console.log(contactoNuevo)
+const modalAdminContacto = new bootstrap.Modal (document.querySelector('#modalContacto'));
+const btnAgregarContacto = document.querySelector('.btn-primary')
+
+//funciones
+const mostrarModal = ()=>{
+    modalAdminContacto.show();
+}
+
+//agregar los manejadores de eventos
+btnAgregarContacto.addEventListener('click', mostrarModal);
+
 
 // crear un contacto
 //abri el modal, completar el formulario
 //submit del form, tomar todos los datos de los input y crear un objeto Contacto
 //luego al contacto lo guardo en un array de contactos
+
+
+
+
+const editarContacto = ()=>{
+    console.log('desde la funcion editar contacto')
+}
