9925c288c0c8fce8ea8e58810fafccebbb18fb1b
Emilse Arias <emilsearias@gmail.com>
2024-08-05 10:37:05 -0300

    creo un contacto que se agrega a la lista de contactos y limpio el formulario

--- js/admin.js
+++ js/admin.js
@@ -1,27 +1,56 @@
 import Contacto from "./classContacto.js";
 
 // crear variables
-const contactoNuevo = new Contacto(undefined,'Rosario', 'Pittaro', 'Ro','algun lugar','381566777', 'rosario@mail.com','link')
-const modalAdminContacto = new bootstrap.Modal (document.querySelector('#modalContacto'));
-const btnAgregarContacto = document.querySelector('.btn-primary')
+
+const modalAdminContacto = new bootstrap.Modal(
+  document.querySelector("#modalContacto")
+);
+const btnAgregarContacto = document.querySelector(".btn-primary");
+const formRegistrarContacto = document.querySelector("form");
+const nombre = document.querySelector("#nombre");
+const apellido = document.querySelector("#apellido");
+const email = document.querySelector("#email"),
+  apodo = document.querySelector("#apodo"),
+  celular = document.querySelector("#celular");
+const foto = document.querySelector("#foto");
+const direccion = document.querySelector("#direccion");
+const listaContactos = [];
 
 //funciones
-const mostrarModal = ()=>{
-    modalAdminContacto.show();
+const mostrarModal = () => {
+  modalAdminContacto.show();
+};
+
+const crearContacto = (e) => {
+  e.preventDefault();
+  // crear un contacto
+  //validar los datos del formulario
+  const contactoNuevo = new Contacto(
+    undefined,
+    nombre.value,
+    apellido.value,
+    apodo.value,
+    direccion.value,
+    celular.value,
+    email.value,
+    foto.value
+  );
+  console.log(contactoNuevo)
+  //agregar el contacto al array
+  listaContactos.push(contactoNuevo);
+  console.log(listaContactos)
+  limpiarFormulario();
+};
+
+const limpiarFormulario = ()=>{
+    formRegistrarContacto.reset();
 }
+
+
+const editarContacto = () => {
+  console.log("desde la funcion editar contacto");
+};
 
 //agregar los manejadores de eventos
-btnAgregarContacto.addEventListener('click', mostrarModal);
-
-
-// crear un contacto
-//abri el modal, completar el formulario
-//submit del form, tomar todos los datos de los input y crear un objeto Contacto
-//luego al contacto lo guardo en un array de contactos
-
-
-
-
-const editarContacto = ()=>{
-    console.log('desde la funcion editar contacto')
-}
+btnAgregarContacto.addEventListener("click", mostrarModal);
+formRegistrarContacto.addEventListener("submit", crearContacto);
