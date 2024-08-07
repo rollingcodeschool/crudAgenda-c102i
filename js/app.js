9c1256c1bdb596b1a022a745da09ac1fca71f74f
Emilse Arias <emilsearias@gmail.com>
2024-08-07 09:22:27 -0300

    agregue la tarea del 07-08

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
-// foto, apodo, direccion
+const listaContactos = JSON.parse(localStorage.getItem("agendaKey")) || [];
+const grilla = document.querySelector(".row");
+console.log("test");
+const cargaInicialTablaContactos = () => {
+  if (listaContactos.length !== 0) {
+    listaContactos.map((contacto) => dibujarFila(contacto));
+  }
+};
+
+const dibujarFila = (contacto) => {
+  grilla.innerHTML += ` <div class="col-md-4 col-lg-3 mb-3">
+    <div class="card h-100">
+            <img
+              src=${contacto.foto}
+              class="card-img-top"
+              alt=${contacto.apellido} ${contacto.nombre}
+            />
+            <div class="card-body">
+              <h5 class="card-title">${contacto.apellido} ${contacto.nombre}</h5>
+              <ul class="list-unstyled">
+                <li><i class="bi bi-person-fill"></i> Apodo: ${contacto.apodo}</li>
+                <li><i class="bi bi-telephone-fill"></i> Celular: ${contacto.celular}</li>
+              </ul>
+             
+            </div>
+             <div class="card-footer text-body-secondary text-end">
+                <button class="btn btn-primary">Ver mas...</button>
+              </div>
+          </div>
+          </div>
+     `;
+};
+
+cargaInicialTablaContactos();
