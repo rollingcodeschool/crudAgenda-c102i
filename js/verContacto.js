df6221a59ab7259e5671c943717eb6ba8aae9143
Emilse Arias <emilsearias@gmail.com>
2024-08-12 09:55:24 -0300

    agregamos el read en verContacto

--- js/verContacto.js
+++ js/verContacto.js
@@ -1,10 +1,34 @@
 //extraer el parametro de la url
-console.log(window.location.search)
-
 const paramId = new URLSearchParams(window.location.search).get('id')
-console.log(paramId)
-
 const listaContactos = JSON.parse(localStorage.getItem("agendaKey")) || [];
+
+function dibujarContacto (){
+    const contenedor = document.querySelector('#contenedorCard')
+    console.log(contenedor);
+    contenedor.innerHTML = `<div class="card mb-3" >
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src=${contactoBuscado.foto} class="img-fluid rounded-start" alt=${contactoBuscado.apodo}>
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">${contactoBuscado.apellido}, ${contactoBuscado.nombre}</h5>
+                <ul class="list-unstyled">
+                  <li>Apodo: ${contactoBuscado.apodo}</li>
+                  <li>Celular: ${contactoBuscado.celular}</li>
+                  <li>Email: ${contactoBuscado.email}</li>
+                  <li>Dirección: ${contactoBuscado.direccion}</li>
+                </ul>
+                </div>
+            </div>
+          </div>
+        </div>`
+}
+
 
 const contactoBuscado = listaContactos.find((contacto)=> contacto.id === paramId);
 console.log(contactoBuscado);
+
+//dibujar contacto
+
+dibujarContacto();
